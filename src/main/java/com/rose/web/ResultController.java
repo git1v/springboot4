69b6bfceb6d7e7625071e2c27be19def76d87fda
dean <git1v@yahoo.co.jp>
2017-07-05 23:34:03 +0900

    input data by Using Form

--- src/main/java/com/rose/web/ResultController.java
+++ src/main/java/com/rose/web/ResultController.java
@@ -1,22 +1,26 @@
 package com.rose.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.rose.web.repositories.MyDataRepository;
 
 @Controller
 public class ResultController {
+	@Autowired
+	MyDataRepository repository;
+	
 	@RequestMapping(value="/", method=RequestMethod.POST)
+	@Transactional(readOnly=false)
 	public ModelAndView send(
-			@RequestParam("name")String name,
-			@RequestParam("mail")String mail,
+			@ModelAttribute("formModel")MyData mydata,
 			ModelAndView mav){
-		String res="Your Name= "+name+" : Your Mail= "+mail;
-		mav.addObject("title","Result");
-		mav.addObject("msg1",res);
-		mav.setViewName("result");
-		return mav;
+		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
 	}
 }
